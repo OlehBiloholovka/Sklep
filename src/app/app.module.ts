b9eff6d5fd3510d22804bad63e7f44105b2bfc5c
OlehBiloholovka <35770198+OlehBiloholovka@users.noreply.github.com>
2019-09-09 14:06:10 +0300

    - added angular material - added firestore - added profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { ProfileComponent } from './profile/profile.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQgroTi7TRCcu0Zy3MT0THAES37nxz6fE',
+  authDomain: 'fly-sklep.firebaseapp.com',
+  databaseURL: 'https://fly-sklep.firebaseio.com',
+  projectId: 'fly-sklep',
+  storageBucket: '',
+  messagingSenderId: '182079410714',
+  appId: '1:182079410714:web:aa13887b7a708ee70ff7a3'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
